0d0f6b7cdd6fc7043ca917668c67cc74d33821bc
Bruck1701 <kummel@gmail.com>
2020-07-28 22:35:27 +0200

    fixed Dockerfile and change PyMarkovChain to markovify

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ from original_data import Data
 from new_data import NewData
 from db import db
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
-
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-
 api.add_resource(Data,"/get/original")
 api.add_resource(NewData,"/get/new_material")
-
 
 if __name__ == "__main__":
     
--- new_data.py
+++ new_data.py
@@ -1,29 +1,32 @@
 from flask_restful import Resource
 from models.sketches import SketchModel
 import random
-from pymarkovchain import MarkovChain
+import markovify
+
 
 class NewData(Resource):
 
 	def get(self):
-		
-		while True:
+		text=''
+		dialogue_size=0
+
+		for i in range(5):
 			index = random.randint(1,45)
 			sketch = SketchModel.find_by_index(index)
-			text = ' '.join(sketch['dialogue'])
-			dialogue_size = len(sketch['dialogue'])
-			if dialogue_size > 5:
-				break
+			text += ' '.join(sketch['dialogue'])
+			dialogue_size += len(sketch['dialogue'])
+		
+		mc = markovify.Text(text)
+		mc = mc.compile()
 
-		if sketch:
-			mc = MarkovChain()
-			mc.generateDatabase(text)
+		result = []
+		for line in range(0, random.randint(1,dialogue_size-1)):
+			sentence = mc.make_sentence()
+			if sentence:
+				result.append(sentence)
 
-			result = []
-			for line in range(0, dialogue_size):
-				result.append(mc.generateString())
-
-			return {'dialogue': result}
-		return {'message':'That was completely Different! :('}
+		return {'dialogue': result}
+		
+		
 
 	
